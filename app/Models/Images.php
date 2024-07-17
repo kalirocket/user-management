cf166d2d41c5eeb3fc820ba25905be016649109b
Enoch Tetteh <124895503+paaqwesilowelltetteh@users.noreply.github.com>
2024-07-17 16:09:42 +0000

    setup users and images migrations and models

---
+++ app/Models/Images.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Images extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'path',
+        'user_id',
+    ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,9 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id')->primary();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('mobile')->unique();
+            $table->enum('status', ['approved', 'rejected', 'suspended']);
+            $table->date('suspended_till')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
